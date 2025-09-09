3a5243fbdab4210165348bef0bd9515b7dcbd6b8
kannaito <221986762+sally8811@users.noreply.github.com>
2025-09-09 18:21:00 +0900

    課題再提出

---
+++ src/Chapter10.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class Chapter10 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		public class Chapter10 {
+		    public static void main(String[] args) throws Exception {
+		        List<Task> list = new ArrayList<>();
+		        list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
+		        list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
+		        list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
+		        list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
+		        list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
+
+		        // 以下記述
+		        
+		        list.stream()
+		              .filter(t -> !t.isDone())
+		              .count();
+		        System.out.println("未完了のタスクの個数は" + );
+		        
+		        
+		        System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
+		        list.stream()
+		            .filter(t -> !isDone())
+		            .sorted()
+		            
+		    }
+		}
+
+	}
+
+}
